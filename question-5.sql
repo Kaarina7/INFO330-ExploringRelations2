997f01239c6364c5f685510342811b8665503fc9
Kaarina <k1j20@tulleau.com>
2023-04-09 14:52:36 -0700

    Fixed question 5

--- question-5.sql
+++ question-5.sql
@@ -1,9 +1,8 @@
 -- What was the most purchased track of 2013?
-SELECT T.Name, COUNT(*) AS TotalPurchases
+SELECT T.Name, SUM(II.Quantity) AS TotalPurchases
 FROM tracks AS T
 	JOIN invoice_items AS II ON T.TrackId = II.TrackId
 	JOIN invoices AS I ON II.InvoiceId = I.InvoiceId
 WHERE I.InvoiceDate BETWEEN '2013-01-01' AND '2013-12-31'
-AND T.Name = 'Fear Of The Dark'
 GROUP BY T.TrackId
-ORDER BY COUNT(*);
+ORDER BY SUM(II.Quantity) DESC;
