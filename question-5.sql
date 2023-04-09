53ae0ce0e64f0300db8883d9fccc80886d589a56
Kaarina <k1j20@tulleau.com>
2023-04-09 10:28:04 -0700

    Finished questions 5-7

--- question-5.sql
+++ question-5.sql
@@ -4,6 +4,6 @@ FROM tracks AS T
 	JOIN invoice_items AS II ON T.TrackId = II.TrackId
 	JOIN invoices AS I ON II.InvoiceId = I.InvoiceId
 WHERE I.InvoiceDate BETWEEN '2013-01-01' AND '2013-12-31'
-AND T.Name = 'Fear Of The Dark';
+AND T.Name = 'Fear Of The Dark'
 GROUP BY T.TrackId
 ORDER BY COUNT(*);
--- question-7.sql
+++ question-7.sql
@@ -1,1 +1,6 @@
 -- Find tracks (id, name, and composer) that have never been purchased (that is, they aren't part of a line item in an invoice).
+SELECT T.TrackId, T.Name, T.Composer
+FROM tracks AS T
+WHERE T.TrackID NOT IN (SELECT T.TrackId
+						FROM tracks AS T
+						JOIN invoice_items AS II ON T.TrackId = II.TrackId);
